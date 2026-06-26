36ea7cb934774d99e017f89a8449ac2f0327ddf3
rachel <rachel058326@gmail.com>
2026-06-26 12:23:37 +0300

    update pipeline

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,15 +1,15 @@
 terraform {
   backend "gcs" {
     bucket                      = "demo-malam"
-    impersonate_service_account = "sa-iac@project-a44aae81-833b-4037-891.iam.gserviceaccount.com"
+    # impersonate_service_account = "sa-iac@project-a44aae81-833b-4037-891.iam.gserviceaccount.com"
     prefix                      = "state"
   }
 }
 provider "google" {
-  impersonate_service_account = "sa-iac@project-a44aae81-833b-4037-891.iam.gserviceaccount.com"
+#   impersonate_service_account = "sa-iac@project-a44aae81-833b-4037-891.iam.gserviceaccount.com"
 }
 provider "google-beta" {
-  impersonate_service_account = "sa-iac@project-a44aae81-833b-4037-891.iam.gserviceaccount.com"
+#   impersonate_service_account = "sa-iac@project-a44aae81-833b-4037-891.iam.gserviceaccount.com"
 }
 
 
