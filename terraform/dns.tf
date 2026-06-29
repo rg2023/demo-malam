b955009b99ed8125a4097f17bea3ccd56b9b3bd6
rachel <rachel058326@gmail.com>
2026-06-29 12:08:16 +0300

    FIX CERT

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -5,26 +5,26 @@ resource "google_compute_global_address" "lb_ip" {
 }
 
 # DNS Zone
-resource "google_dns_managed_zone" "demoapp" {
-  name        = "demoapp-zone"
-  dns_name    = "demoapp.com."
-  project     = var.project_id
-  visibility  = "public"
-}
+# resource "google_dns_managed_zone" "demoapp" {
+#   name        = "demoapp-zone"
+#   dns_name    = "demoapp.com."
+#   project     = var.project_id
+#   visibility  = "public"
+# }
 
-# DNS Record
-resource "google_dns_record_set" "demoapp" {
-  name         = "demoapp.com."
-  type         = "A"
-  ttl          = 300
-  managed_zone = google_dns_managed_zone.demoapp.name
-  project      = var.project_id
-  rrdatas      = [google_compute_global_address.lb_ip.address]
-}
+# # DNS Record
+# resource "google_dns_record_set" "demoapp" {
+#   name         = "demoapp.com."
+#   type         = "A"
+#   ttl          = 300
+#   managed_zone = google_dns_managed_zone.demoapp.name
+#   project      = var.project_id
+#   rrdatas      = [google_compute_global_address.lb_ip.address]
+# }
 resource "google_compute_managed_ssl_certificate" "demoapp" {
-  name    = "demoapp-cert"
+  name    = "demoapp-cert-${terraform.workspace}"
   project = var.project_id
   managed {
-    domains = ["demoapp.com"]
+    domains = [terraform.workspace == "prod" ? "demoapp.com" : "dev.demoapp.com"]
   }
 }
